42efcd0375d1e7dc2f6639c48078c83c2fa57dea
vk1ddd <ivanuxas@gmail.com>
2025-12-13 14:34:38 +0300

    add simple logic

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Account, Category, Transaction, Budget
 
-# Register your models here.
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(Transaction)
+admin.site.register(Budget)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Transaction, Account, Category
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def dashboard(request):
+    transactions = Transaction.objects.filter(user=request.user).order_by('-timestamp')
+    context = {
+        'transactions': transactions
+    }
+    return render(request, 'core/dashboard.html', context)
+
+@login_required
+def add_transaction(request):
+    if request.method == 'POST':
+        account_id = request.POST['account']
+        category_id = request.POST['category']
+        amount = request.POST['amount']
+        comment = request.POST['comment']
+
+        account = Account.objects.get(id=account_id)
+        category = Category.objects.get(id=category_id)
+
+        Transaction.objects.create(
+            user=request.user,
+            account=account,
+            category=category,
+            amount=amount,
+            comment=comment
+        )
+
+        if category.type == 'EXPENSE':
+            account.balance -= float(amount)
+        elif category.type == 'INCOME':
+            account.balance += float(amount)
+        account.save()
+
+        return redirect('dashboard')
+    else:
+        accounts = Account.objects.filter(user=request.user)
+        categories = Category.objects.filter(user=request.user)
+        context = {
+            'accounts': accounts,
+            'categories': categories,
+        }
+        return render(request, 'core/add_transaction.html', context)
